dce36e4a3153a007c340ce467dd929b4e7156380
Sturmschwalbe84 <ivanlevitan0600@gmail.com>
2021-10-20 00:27:11 +0300

    qwe

--- golang-app/main.go
+++ golang-app/main.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello World 2")
+		c.String(200, "Hello World 22")
 	})
 	r.POST("/", func(c *gin.Context) {
 		c.String(200, "Hello World 2")
