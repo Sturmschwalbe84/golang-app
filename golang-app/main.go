969d3292faa675ed7db0d7132ae864e805cc2fca
Sturmschwalbe84 <92655657+Sturmschwalbe84@users.noreply.github.com>
2021-10-20 23:21:24 +0300

    Update main.go

--- golang-app/main.go
+++ golang-app/main.go
@@ -9,10 +9,10 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello World 2.1")
+		c.String(200, "Hello World 2.2")
 	})
 	r.POST("/", func(c *gin.Context) {
-		c.String(200, "Hello World 2.1")
+		c.String(200, "Hello World 2.2")
 	})
 	err := r.Run(":8080")
 	if err != nil {
